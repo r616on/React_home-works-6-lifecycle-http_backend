b6eee895021568ae2ed17d75231bb40b5b46af4a
r616on <r616on@gmail.com>
2021-12-01 22:33:51 +0300

    init

---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const Koa = require("koa");
+const Router = require("koa-router");
+const cors = require("koa2-cors");
+const koaBody = require("koa-body");
+const { v4: uuidv4 } = require("uuid");
+
+const app = new Koa();
+
+app.use(cors());
+app.use(koaBody({ json: true }));
+
+const notes = [
+  { id: uuidv4(), content: "Первая задача, что то там надо выполнить" },
+  { id: uuidv4(), content: "Вторая задача, что то там надо выполнить" },
+];
+
+const router = new Router();
+
+router.get("/notes", async (ctx, next) => {
+  ctx.response.body = notes;
+});
+
+router.post("/notes", async (ctx, next) => {
+  notes.push(JSON.parse(ctx.request.body));
+  ctx.response.status = 204;
+});
+
+router.delete("/notes/:id", async (ctx, next) => {
+  const noteId = ctx.params.id;
+  const index = notes.findIndex((o) => o.id === noteId);
+  if (index !== -1) {
+    notes.splice(index, 1);
+  }
+  ctx.response.status = 204;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = process.env.PORT || 7777;
+const server = http.createServer(app.callback());
+server.listen(port, () => console.log("server started"));
